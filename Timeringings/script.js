47ec25b7c1a726df7d5897fdc5981e24e20dfef6
Soham Ghosh <sgggz217@gmail.com>
2026-09-10 22:56:01 +0530

    basic files of the webapp
    i plan on making this timer a weekly scheduled timer, which is very customizable etc etc

---
+++ Timeringings/script.js
@@ -0,0 +1,72 @@
+const minutesInput = document.getElementById('minis');
+const secondsInput = document.getElementById('seccy');
+const startBtn = document.getElementById('str-pau');
+const resetBtn = document.getElementById('res-abrt');
+
+let timerInterval = null;
+let isRunning = false;
+let totalSeconds = 0;
+
+
+function updateDisplay(){
+  const mins = Math.floor(totalSeconds / 60)
+  const secs = totalSeconds % 60;
+  minutesInput.value = String(mins).padStart(2, '0');
+  secondsInput.value = String(secs).padStart(2, '0');
+}
+
+function getTotalSeconds(){
+  const mins = parseInt(minutesInput.value) || 0;
+  const secs = parseInt(secondsInput.value) || 0;
+  return mins * 60 + secs;
+}
+
+function startTimer(){
+  if (isRunning) return;
+  totalSeconds = getTotalSeconds();
+  if (totalSeconds <= 0) return;
+  
+  isRunning = true;
+  startBtn.textContent = '⏸️'
+
+  timerInterval = setInterval(() => {
+    if (totalSeconds <= 0) {
+      clearInterval(timerInterval);
+      timerInterval = null;
+      isRunning = false;
+      startBtn.textContent = '▶️';
+      alert ('timess uppp');
+      return
+    }
+    totalSeconds--;
+    updateDisplay();
+  }, 1000);  
+}
+
+function pauseTimer(){
+  clearInterval(timerInterval);
+  timerInterval = null;
+  isRunning = false;
+  startBtn.textContent = '▶️';
+}
+
+function resetTimer(){
+  clearInterval(timerInterval);
+  timerInterval = null;
+  isRunning = false;
+  startBtn.textContent = '▶️';
+  totalSeconds = 0;
+  updateDisplay();
+}
+
+startBtn.addEventListener('click', () => {
+  if(isRunning) {
+    pauseTimer();
+  } else {
+    startTimer();
+  }
+});
+
+resetBtn.addEventListener('click', resetTimer);
+
+updateDisplay();
